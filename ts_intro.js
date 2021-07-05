cba4539db1d7383b812f681a0ac5f48d051f7566
levequesa <sandrine.leveque@natixis.com>
2021-07-05 10:41:42 +0200

    challenge1 - TS intro

---
+++ ts_intro.js
@@ -0,0 +1,13 @@
+//Ajout typage string
+function hello(name) {
+    console.log("Hello " + name);
+}
+var firstName = "bob";
+hello(firstName);
+hello(firstName + " marley");
+//Ajout typage String
+function concat(a, b) {
+    return a + b;
+}
+var wcs = concat("Wild", concat("Code", "School"));
+console.log(wcs);
